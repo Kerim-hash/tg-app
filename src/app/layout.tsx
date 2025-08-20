722b6b2004fed3971bf5ff84f754dbc589b43147
kerim <kerim.nurkaliev@inbox.ru>
2025-08-20 19:19:53 +0600

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,27 @@
-"use client";
-
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { ReactNode } from "react";
+import Script from "next/script";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Telegram Mini App",
+  description: "Получение данных пользователя в Telegram Mini App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body>
-       {children}
+    <html lang="ru" suppressHydrationWarning>
+      <head>
+        <Script src="https://telegram.org/js/telegram-web-app.js?56" strategy="beforeInteractive" />
+      </head>
+      <body className={inter.className} suppressHydrationWarning>
+        {children}
       </body>
     </html>
   );
